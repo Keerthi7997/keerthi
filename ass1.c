143505cede0b26293ad78b96668e94b4c1127e90
Keerthi7997 <64069494+Keerthi7997@users.noreply.github.com>
2021-02-22 15:55:29 +0530

    Add files via upload

---
+++ ass1.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<math.h>
+main()
+{
+class product{
+		public int maxProduct(int[]nums){
+			int maxA=num[0];
+			int maxB=num[0];
+			int min=num[0];
+			for(int i=1;i<num,length;i++){
+				int min=min*num[i];
+				int maxB=maxB*num[i];
+				min=math.min(math.min(num[i],min),maxB);
+				maxB=math.maxB(math.maxB(nums[i],min),maxB);
+				if(maxB>maxA)maxA=maxB;
+			}
+			return maxA;
+		}
+	}
+}
